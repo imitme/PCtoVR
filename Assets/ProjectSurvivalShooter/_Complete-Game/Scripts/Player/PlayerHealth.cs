705aa07096eccf342fc2b3ba7b6c06bf9a048f21
min <7kyeongmin@gmail.com>
2019-01-09 19:18:28 +0900

    총기 발사

--- Assets/ProjectSurvivalShooter/_Complete-Game/Scripts/Player/PlayerHealth.cs
+++ Assets/ProjectSurvivalShooter/_Complete-Game/Scripts/Player/PlayerHealth.cs
@@ -15,32 +15,29 @@ namespace CompleteProject
         public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
         public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
 
+        private Animator anim;                                              // Reference to the Animator component.
+        private AudioSource playerAudio;                                    // Reference to the AudioSource component.
+        private PlayerMovement playerMovement;                              // Reference to the player's movement.
+        private PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+        private bool isDead;                                                // Whether the player is dead.
+        private bool damaged;                                               // True when the player gets damaged.
 
-        Animator anim;                                              // Reference to the Animator component.
-        AudioSource playerAudio;                                    // Reference to the AudioSource component.
-        PlayerMovement playerMovement;                              // Reference to the player's movement.
-        PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
-        bool isDead;                                                // Whether the player is dead.
-        bool damaged;                                               // True when the player gets damaged.
-
-
-        void Awake ()
+        private void Awake()
         {
             // Setting up the references.
-            anim = GetComponent <Animator> ();
-            playerAudio = GetComponent <AudioSource> ();
-            playerMovement = GetComponent <PlayerMovement> ();
-            playerShooting = GetComponentInChildren <PlayerShooting> ();
+            anim = GetComponent<Animator>();
+            playerAudio = GetComponent<AudioSource>();
+            playerMovement = GetComponent<PlayerMovement>();
+            playerShooting = GetComponentInChildren<PlayerShooting>();
 
             // Set the initial health of the player.
             currentHealth = startingHealth;
         }
 
-
-        void Update ()
+        private void Update()
         {
             // If the player has just been damaged...
-            if(damaged)
+            if (damaged)
             {
                 // ... set the colour of the damageImage to the flash colour.
                 damageImage.color = flashColour;
@@ -49,15 +46,14 @@ namespace CompleteProject
             else
             {
                 // ... transition the colour back to clear.
-                damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+                damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
             }
 
             // Reset the damaged flag.
             damaged = false;
         }
 
-
-        public void TakeDamage (int amount)
+        public void TakeDamage(int amount)
         {
             // Set the damaged flag so the screen will flash.
             damaged = true;
@@ -69,42 +65,40 @@ namespace CompleteProject
             healthSlider.value = currentHealth;
 
             // Play the hurt sound effect.
-            playerAudio.Play ();
+            playerAudio.Play();
 
             // If the player has lost all it's health and the death flag hasn't been set yet...
-            if(currentHealth <= 0 && !isDead)
+            if (currentHealth <= 0 && !isDead)
             {
                 // ... it should die.
-                Death ();
+                Death();
             }
         }
 
-
-        void Death ()
+        private void Death()
         {
             // Set the death flag so this function won't be called again.
             isDead = true;
 
             // Turn off any remaining shooting effects.
-            playerShooting.DisableEffects ();
+            playerShooting.DisableEffects();
 
             // Tell the animator that the player is dead.
-            anim.SetTrigger ("Die");
+            anim.SetTrigger("Die");
 
             // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
             playerAudio.clip = deathClip;
-            playerAudio.Play ();
+            playerAudio.Play();
 
             // Turn off the movement and shooting scripts.
-            playerMovement.enabled = false;
+            //playerMovement.enabled = false;
             playerShooting.enabled = false;
         }
 
-
-        public void RestartLevel ()
+        public void RestartLevel()
         {
             // Reload the level that is currently loaded.
-            SceneManager.LoadScene (0);
+            SceneManager.LoadScene(0);
         }
     }
 }
